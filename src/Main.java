7be546acf57ee54627ab0c9b159e4f1345935739
Ashok Dhungana <you@example.com>
2023-11-05 13:42:07 +0545

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,82 @@
+package cylindertest;
 
- class Cylinder{
-    public double radius,height;
+class  Cylinder
+{
+    private int radius;
+    private int height;
 
-    public double lidArea(){
-        return 2*Math.PI*radius*height+2*Math.PI*radius*radius ;
+    public Cylinder()
+    {
+        radius=height=1;
+    }
+    public Cylinder(int r,int h)
+    {
+        radius=r;
+        height=h;
+    }
+    public int getHeight()
+    {
+        return height;
+    }
+    public int getRadius()
+    {
+        return radius;
     }
 
-    public double totalSurfaceArea(){
-        return lidArea();
+    public void setHeight(int h)
+    {
+        if(h>=0)
+            height=h;
+        else
+            height=0;
     }
-    public double volume(){
-        return Math.PI*radius*radius*height ;
+    public void setRadius(int r)
+    {
+        if(r>=0)
+            radius=r;
+        else
+            radius=0;
     }
- }
+    public void setDimensions(int h,int r)
+    {
+        height=h;
+        radius=r;
+    }
 
-  public class Main{
-      public static void main(String[] args) {
-          Cylinder c = new Cylinder();
-          c.radius=12;
-          c.height=13;
-          System.out.println();
-      }
-  }
+    public double lidArea()
+    {
+        return Math.PI*radius*radius;
+    }
+    public double perimeter()
+    {
+        return 2*Math.PI*radius;
+    }
+    public double drumArea()
+    {
+        return 2*lidArea()+perimeter()*height;
+    }
+    public double volume()
+    {
+        return lidArea()*height;
+    }
+}
+
+
+public class CylinderTest {
+
+    public static void main(String[] args) {
+        Cylinder c=new Cylinder();
+        c.setHeight(10);
+        c.setRadius(7);
+        c.setDimensions(10, 7);
+
+        System.out.println("LidArea "+c.lidArea());
+        System.out.println("Circumference "+c.perimeter());
+        System.out.println("totalSurfaceArea "+c.drumArea());
+        System.out.println("Volume "+c.volume());
+        System.out.println("Height"+c.getHeight());
+        System.out.println("Radius"+c.getRadius());
+
+    }
+
+}
